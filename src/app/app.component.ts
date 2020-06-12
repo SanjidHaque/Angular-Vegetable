ea0aa2336027f9ce3244f0dc39aad567e6a4bd59
SanjidHaque <sanjidulhaque@gmail.com>
2020-06-12 10:13:25 +0600

    Finishing up the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-vegetable';
+export class AppComponent implements OnInit {
+  vegetables: any[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.httpClient.get('https://test-schema.herokuapp.com/vegetables')
+      .subscribe((response: any) => {
+        this.vegetables = response.data;
+        this.veggieCalculation();
+      });
+  }
+
+  veggieCalculation() {
+    const totalPrice = this.vegetables.reduce((sum, veggie) => sum + veggie.price, 0);
+    console.log('Total price: ' + totalPrice);
+
+    this.vegetables.map((veggie, index) => {
+      this.vegetables[index].price = (veggie.price * .15) + veggie.price;
+    });
+    console.log(this.vegetables);
+
+    const filterVeggies = this.vegetables.filter(x => x.price > 50);
+    console.log(filterVeggies);
+  }
 }
